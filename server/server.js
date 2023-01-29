2ac3776058cb352b0ae22cc730e1c941f43b13b2
vpsines <sasip6028@gmail.com>
2023-01-29 19:10:25 +0530

    feat: add api/signup route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config(); 
+
+// imports from other packages
+const authRouter = require('./routes/auth.js');
 
 const PORT=3000;
-
 const app = express();
+const mongoUri = process.env.MONGO_URI;
 
-// 
-app.get('/', function(req,res){
+// middleware
+app.use(express.json());
+app.use(authRouter);
 
+mongoose.connect(mongoUri).then(()=>{
+    console.log("Connected successfullly..");
+}).catch((err)=>{
+    console.log(err);
 });
 
 app.listen(PORT,"0.0.0.0",()=>{
