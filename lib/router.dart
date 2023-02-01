f5bcda98c6dceb3beb31da2d6fb761aede3fd579
vpsines <sasip6028@gmail.com>
2023-02-01 12:53:43 +0530

    feat: add homescreen

--- lib/router.dart
+++ lib/router.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:my_amazon/screens/auth/auth_screen.dart';
+import 'package:my_amazon/screens/home/home_screen.dart';
 
 Route<dynamic> generateRoutes(RouteSettings routeSettings) {
   switch (routeSettings.name) {
     case AuthScreen.routeName:
       return MaterialPageRoute(
           settings: routeSettings, builder: (_) => const AuthScreen());
+    case HomeScreen.routeName:
+      return MaterialPageRoute(settings:routeSettings,builder: (_)=>const HomeScreen());
     default:
       return MaterialPageRoute(
         settings: routeSettings,
