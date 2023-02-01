d09fcefd0df68e0c7aff6bd8faf309f18603a492
vpsines <sasip6028@gmail.com>
2023-02-01 14:41:24 +0530

    feat: add auth middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const auth = async function(req,res,next){
+    try{
+        const token = req.header['x-auth-token'];
+
+        if(!token) return res.status(401).json({msg:'User not authorized'});
+
+        const isVerified =  jwt.verify(token,'passwordKey');
+
+        if(!isVerified) return res.status(401).json({msg:'Token verification failed, authorization denied.'});
+
+        req.id = isVerified.id;
+        req.token = token;
+        next();
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }
+}
+
+module.exports = auth;
