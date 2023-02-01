2489ea148b0b85818abf739b4ed64ecc361dd0b9
vpsines <sasip6028@gmail.com>
2023-02-01 10:56:30 +0530

    feat: add sigin route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require('../models/user.js');
 const bcryptjs = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const authRouter = express.Router();
 
 // sign up api
@@ -29,6 +29,34 @@ authRouter.post('/api/signup',async function(req,res){
     user = await user.save();
 
     res.json(user); 
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+});
+
+// sign in api
+authRouter.post('/api/signin',async function(req,res){
+    try {
+        // get the data from the client
+    const {email, password} = req.body;
+
+    // check if user already exists
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(400).json({msg:"User not found"});
+    }
+    
+    // check if password matches 
+    const isMatch = await bcryptjs.compare(password,user.password);
+
+    if(!isMatch){
+        return res.status(400).json({msg:"Incorrect password"})
+    }
+
+    const token = jwt.sign({id:user._id},"passwordKey");
+    res.json({token,...user._doc});
+
     } catch (error) {
         res.status(500).json({error:error.message});
     }
