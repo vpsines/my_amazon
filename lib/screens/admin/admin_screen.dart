4043618ffdb417249f17a03ab27dd9adae29e773
vpsines <sasip6028@gmail.com>
2023-02-06 10:31:20 +0530

    feat: add admin screen

---
+++ lib/screens/admin/admin_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:my_amazon/configs/themes/ui_parameters.dart';
+import 'package:my_amazon/constants/app_colors.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({super.key});
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+
+    int _page = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+
+  List<Widget> pages = [
+    const Center(
+      child: Text("Posts"),
+    ),    const Center(
+      child: Text("Analytics"),
+    ),
+    const Center(
+      child: Text("Cart"),
+    ),
+  ];
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(50),
+            child: AppBar(
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                  gradient: UiParameters.appBarGradient)
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Image.asset("assets/images/amazon_in.png",
+                      width: 120,
+                      height: 45,
+                      color: Colors.black,),
+                    ),
+                    const Text(
+                      'Admin',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+                  body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: updatePage,
+        currentIndex: _page,
+        selectedItemColor: AppColors.selectedNavBarColor,
+        unselectedItemColor: AppColors.unselectedNavBarColor,
+        backgroundColor: AppColors.backgroundColor,
+        iconSize: 28,
+        items: [
+          BottomNavigationBarItem(
+              icon: Container(
+                width: bottomBarWidth,
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(
+                  color: _page == 0
+                      ? AppColors.selectedNavBarColor
+                      : AppColors.backgroundColor,
+                  width: bottomBarBorderWidth,
+                ))),
+                child: const Icon(Icons.home_outlined),
+              ),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Container(
+                width: bottomBarWidth,
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(
+                  color: _page == 1
+                      ? AppColors.selectedNavBarColor
+                      : AppColors.backgroundColor,
+                  width: bottomBarBorderWidth,
+                ))),
+                child: const Icon(Icons.analytics_outlined),
+              ),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Container(
+                width: bottomBarWidth,
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(
+                  color: _page == 2
+                      ? AppColors.selectedNavBarColor
+                      : AppColors.backgroundColor,
+                  width: bottomBarBorderWidth,
+                ))),
+                child: const Icon(Icons.all_inbox_outlined),
+              ),
+              label: ''),
+        ],
+      ),
+    );
+  }
+}
