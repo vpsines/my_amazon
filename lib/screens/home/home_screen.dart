ee71c104bf3c2fdb662c797190b933b14a4311c6
vpsines <sasip6028@gmail.com>
2023-02-06 10:30:46 +0530

    feat: update home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_amazon/configs/themes/ui_parameters.dart';
 import 'package:my_amazon/providers/user_provider.dart';
+import 'package:my_amazon/widgets/home/address_box.dart';
+import 'package:my_amazon/widgets/home/carousel_image.dart';
+import 'package:my_amazon/widgets/home/deal_of_day.dart';
+import 'package:my_amazon/widgets/home/top_categories.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = '/home-screen';
-  
+
   const HomeScreen({super.key});
 
   @override
@@ -17,9 +22,85 @@ class _HomeScreenState extends State<HomeScreen> {
     final user = Provider.of<UserProvider>(context).user;
 
     return Scaffold(
-      body: Center(
-        child: Text(user.toString()),
-      ),
-    );
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: AppBar(
+            flexibleSpace: Container(
+                decoration:
+                    const BoxDecoration(gradient: UiParameters.appBarGradient)),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 42,
+                    margin: const EdgeInsets.only(left: 15),
+                    child: Material(
+                      elevation: 1,
+                      borderRadius: BorderRadius.circular(7),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: InkWell(
+                              onTap: () {},
+                              child: const Padding(
+                                padding: EdgeInsets.only(left: 6),
+                                child: Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                  size: 23,
+                                ),
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: const EdgeInsets.only(top: 10),
+                            border: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7)),
+                                borderSide: BorderSide.none),
+                            enabledBorder: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7)),
+                                borderSide: BorderSide(
+                                    color: Colors.black38, width: 1)),
+                            hintText: 'Search Amazon.in',
+                            hintStyle: const TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 17)),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  color: Colors.transparent,
+                  height: 42,
+                  child: const Icon(
+                    Icons.mic,
+                    size: 25,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: const [
+              AddressBox(),
+              SizedBox(
+                height: 10,
+              ),
+              TopCategories(),
+              SizedBox(
+                height: 10,
+              ),
+              CarouselImage(),
+              DealOfDay()
+            ],
+          ),
+        ));
   }
 }
