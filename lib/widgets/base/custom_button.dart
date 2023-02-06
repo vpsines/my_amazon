a01be1dc749d38ad8132bc3fe714095c7826d29d
vpsines <sasip6028@gmail.com>
2023-02-06 19:03:09 +0530

    feat: add color field for custom button

--- lib/widgets/base/custom_button.dart
+++ lib/widgets/base/custom_button.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-
   final VoidCallback onTap;
   final String buttonText;
-  
-  const CustomButton({super.key, required this.onTap, required this.buttonText});
+  final Color? color;
+  const CustomButton(
+      {super.key, required this.onTap, required this.buttonText, this.color});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
-        minimumSize: const Size(double.infinity, 50)
+          minimumSize: const Size(double.infinity, 50), backgroundColor: color),
+      child: Text(
+        buttonText,
+        style: TextStyle(color: color ?? Colors.white),
       ),
-      child: Text(buttonText),
     );
   }
 }
