299fbd11af91d6216ef38d34b14c2c1ea4b43cd9
vpsines <sasip6028@gmail.com>
2023-02-06 11:20:28 +0530

    feat: add category deal screen route

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_amazon/screens/admin/add_product_screen.dart';
 import 'package:my_amazon/screens/auth/auth_screen.dart';
+import 'package:my_amazon/screens/home/category_deals_screen.dart';
 import 'package:my_amazon/screens/home/home_screen.dart';
 import 'package:my_amazon/widgets/base/bottom_bar.dart';
 
@@ -18,6 +19,13 @@ Route<dynamic> generateRoutes(RouteSettings routeSettings) {
     case AddProductScreen.routeName:
       return MaterialPageRoute(
           settings: routeSettings, builder: (_) => const AddProductScreen());
+    case CategoryDealsScreen.routeName:
+      var category = routeSettings.arguments as String;
+      return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => CategoryDealsScreen(
+                category: category,
+              ));
     default:
       return MaterialPageRoute(
           settings: routeSettings,
