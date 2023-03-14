b00f2558a8e9fb7856e82f5d209270c1d0672907
vpsines <sasip6028@gmail.com>
2023-03-14 23:10:20 +0530

    feat: add product schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const ratingSchema = require('./rating');
 
 const productSchema = mongoose.Schema(
     {
@@ -30,9 +31,10 @@ const productSchema = mongoose.Schema(
             type:String,
             required:true
         },
+        ratings:[ratingSchema]
     }
 );
 
 const Product = mongoose.model("Product",productSchema);
 
-module.exports = Product;
+module.exports = {Product, productSchema};
