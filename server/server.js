e8764bf85597db3d246f42c19d88a920afee118f
vpsines <sasip6028@gmail.com>
2023-03-17 23:38:50 +0530

    feat: add routers

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 
 // imports from other packages
 const authRouter = require('./routes/auth.js');
+const adminRouter = require('./routes/admin.js');
+const productRouter = require('./routes/product.js');
+const userRouter = require('./routes/user.js');
 
 const PORT=3000;
 const app = express();
@@ -12,6 +15,9 @@ const mongoUri = process.env.MONGO_URI;
 // middleware
 app.use(express.json());
 app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
 
 mongoose.connect(mongoUri).then(()=>{
     console.log("Connected successfullly..");
